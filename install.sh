b1d64ddc60d4df992cc7d71d5bc7ed9a805581a3
Petri Damstén <petri.damsten@gmail.com>
2025-06-09 09:41:59 +0300

    fix app budle path fix

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ APP="$(pwd)/flash-control.py"
 CMD="\"$PYTHON\" \"$APP\""
 WFLOW="$(pwd)/Flash Control.app/Contents/document.wflow"
 
-ESCAPED_COMMAND=$(printf "%s" "$CMD" | sed 's/"/\\\"/g' | awk '{printf "%s\\n", $0}')
+echo $CMD
+ESCAPED_COMMAND=$(printf "%s" "$CMD" | sed 's/"/\\\"/g' | awk '{printf "%s", $0}')
 echo $ESCAPED_COMMAND
 sed -i '' -E "/<key>COMMAND_STRING<\/key>/{n;s|<string>.*</string>|<string>$ESCAPED_COMMAND</string>|;}" "$WFLOW"
