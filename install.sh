7d1497865c23c06eb7b5d2b405842843de6638d0
Petri Damstén <petri.damsten@gmail.com>
2025-06-09 09:42:57 +0300

    more verbose

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Depedencies installation
+echo "Installing dependencies..."
 
 python -m pip install -r dependencies-macos.txt
 
-# Update the workflow file to use the new command string
+echo "Updating workflow file in app bundle..."
 
 PYTHON=$(which python3)
 APP="$(pwd)/flash-control.py"
@@ -13,5 +13,5 @@ WFLOW="$(pwd)/Flash Control.app/Contents/document.wflow"
 
 echo $CMD
 ESCAPED_COMMAND=$(printf "%s" "$CMD" | sed 's/"/\\\"/g' | awk '{printf "%s", $0}')
-echo $ESCAPED_COMMAND
+#echo $ESCAPED_COMMAND
 sed -i '' -E "/<key>COMMAND_STRING<\/key>/{n;s|<string>.*</string>|<string>$ESCAPED_COMMAND</string>|;}" "$WFLOW"
